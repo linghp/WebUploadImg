3964953dac0a45c7c8998aa9e98e8a45b909fde4
linghuanpeng <huanpeng.ling@msxf.com>
2017-11-07 09:42:35 +0800

    cancel upload

--- WebUploadImg/app/src/main/java/com/jarry/webuploadimg/MainActivity.java
+++ WebUploadImg/app/src/main/java/com/jarry/webuploadimg/MainActivity.java
@@ -7,68 +7,66 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.Toast;
-
 import com.jarry.webuploadimg.widget.ProgressWebView;
 
 public class MainActivity extends AppCompatActivity {
-    ProgressWebView webView;
-    static final String TAG = "MainActivity";
-    final String url = "https://test.doraemoney.com/newCube/SourceTestPage.html";
+  ProgressWebView webView;
+  static final String TAG = "MainActivity";
+  //final String url = "https://test.doraemoney.com/newCube/SourceTestPage.html";
+  final String url =
+      "http://vayh10.msxf.lotest/zxhapp/home_index?vtoken=MSJR-APP_4420ab58-da3a-4d53-b142-8f2e3225b064&channel=MSJR_sbddl&productCode=5204";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        webView = (ProgressWebView) findViewById(R.id.webview);
-        initData();
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    webView = (ProgressWebView) findViewById(R.id.webview);
+    initData();
+  }
+
+  private void initData() {
+    webView.loadUrl(url);
+  }
+
+  public boolean onKeyDown(int keyCode, KeyEvent event) {
+    if (keyCode == KeyEvent.KEYCODE_BACK) {
+      if (webView.canGoBack()) {
+        webView.goBack();// 返回上一页面
+        return true;
+      } else {
+        this.finish();
+      }
     }
+    return super.onKeyDown(keyCode, event);
+  }
 
-    private void initData() {
-        webView.loadUrl(url);
-    }
-
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            if (webView.canGoBack()) {
-                webView.goBack();// 返回上一页面
-                return true;
-            } else {
-                this.finish();
-            }
+  @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    Log.d(TAG, "requestCode=" + requestCode);
+    if (resultCode == RESULT_OK) {
+      if (requestCode == ProgressWebView.TYPE_CAMERA) { // 相册选择
+        webView.onActivityCallBack(true, null);
+      } else if (requestCode == ProgressWebView.TYPE_GALLERY) {// 相册选择
+        if (data != null) {
+          Uri uri = data.getData();
+          Log.d(TAG, "uri=" + uri);
+          if (uri != null) {
+            webView.onActivityCallBack(false, uri);
+          } else {
+            Toast.makeText(MainActivity.this, "获取数据为空", Toast.LENGTH_LONG).show();
+          }
         }
-        return super.onKeyDown(keyCode, event);
+      }
+    } else {
+      webView.cancel();
     }
+  }
 
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.d(TAG, "requestCode=" + requestCode);
-        if (resultCode == RESULT_OK) {
-            if (requestCode == ProgressWebView.TYPE_CAMERA) { // 相册选择
-                webView.onActivityCallBack(true, null);
-            } else if (requestCode == ProgressWebView.TYPE_GALLERY) {// 相册选择
-                if (data != null) {
-                    Uri uri = data.getData();
-                    Log.d(TAG, "uri=" + uri);
-                    if (uri != null) {
-                        webView.onActivityCallBack(false, uri);
-                    } else {
-                        Toast.makeText(MainActivity.this, "获取数据为空", Toast.LENGTH_LONG).show();
-                    }
-                }
-            }
-
-        }
+  // 权限回调
+  @Override public void onRequestPermissionsResult(int requestCode, String[] permissions,
+      int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    if (requestCode == ProgressWebView.TYPE_REQUEST_PERMISSION) {
+      webView.toCamera();// 到相机
     }
-
-    // 权限回调
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == ProgressWebView.TYPE_REQUEST_PERMISSION) {
-            webView.toCamera();// 到相机
-        }
-    }
+  }
 }
 
